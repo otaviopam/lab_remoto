4006f3ee638f4b15d7f5ff341dba785d889c4abd
otaviopam <otaviopam@gmail.com>
2013-07-05 18:11:28 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<head>
+<title>teste</title>
+<script src="include/js/jquery.js"></script>
+<!-- <script src="include/js/underscore.js"></script> -->
+<!-- <script src="include/js/backbone.js"></script> -->
+
+<script src="main/main.js"></script>
+</head>
+<body>
+
+	<div id="dataStore">&nbsp;</div>
+
+	<input type="button" onclick="javascript:getConteudo()">
+
+</body>
+</html>
---
+++ main/main.js
@@ -0,0 +1,22 @@
+$(function() {
+	getFeed('http://sindmetau.org.br/site/index.php?format=feed&type=rss');
+});
+
+function parseRSS(url, callback) {
+  $.ajax({
+    url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+    dataType: 'json',
+    success: function(data) {
+      callback(data.responseData.feed);
+    }
+  });
+}
+
+function getFeed(url) {
+	parseRSS(url,function(retorno) { getConteudo(retorno); });
+}
+
+function getConteudo(retorno) {
+	console.debug($(retorno).find('item'));
+} 
+
