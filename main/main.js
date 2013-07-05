1a9d1a6205d233fcce062e96aa8dcf50106ce10a
otaviopam <otaviopam@gmail.com>
2013-07-05 19:41:18 -0300

    Commit inicial

--- main/main.js
+++ main/main.js
@@ -2,14 +2,21 @@ $(function() {
 	getFeed('http://sindmetau.org.br/site/index.php?format=feed&type=rss');
 });
 
+$(document).ready(function () {
+	$('#grid').dataTable({
+		"sPaginationType": "full_numbers"
+	});
+	
+})
+
 function parseRSS(url, callback) {
-  $.ajax({
-    url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-    dataType: 'json',
-    success: function(data) {
-      callback(data.responseData.feed);
-    }
-  });
+	$.ajax({
+	    url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+	    dataType: 'json',
+	    success: function(data) {
+	    	callback(data.responseData.feed);
+	    }
+	});
 }
 
 function getFeed(url) {
