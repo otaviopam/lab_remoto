c47c6eb29be7ef69e6e1696e8ff536f401577984
otaviopam <otaviopam@gmail.com>
2013-07-10 18:48:54 -0300

    fully working jQuery RSS + DataTable

--- main/main.js
+++ main/main.js
@@ -1,20 +1,23 @@
 $(function() {
-	getFeed('http://sindmetau.org.br/site/index.php?format=feed&type=rss');
+	//getFeed('http://sindmetau.org.br/site/index.php?format=feed&type=rss');
+	getFeed('http://feeds.arstechnica.com/arstechnica/index?format=xml');
+	
 });
 
-$(document).ready(function () {
-	$('#grid').dataTable({
-		"sPaginationType": "full_numbers"
-	});
-	
-})
+//$(document).ready(function () {
+//	$('#grid').dataTable({
+//		"sPaginationType": "full_numbers"
+//	});
+//	
+//})
 
 function parseRSS(url, callback) {
 	$.ajax({
-	    url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+		//&num=10
+	    url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=?&q=' + encodeURIComponent(url),
 	    dataType: 'json',
 	    success: function(data) {
-	    	callback(data.responseData.feed);
+	    	callback(data.responseData.feed.entries);
 	    }
 	});
 }
@@ -24,6 +27,31 @@ function getFeed(url) {
 }
 
 function getConteudo(retorno) {
-	console.debug($(retorno).find('item'));
+	//console.debug($(retorno));
+//	console.debug(JSON.stringify($(retorno),null,2));
+
+	var objRetorno = $(retorno);
+	var aaData=[];
+	
+//	console.debug(objRetorno);
+	
+	$.each(objRetorno, function(i,val) {
+		aaData.push([val.title,val.content]);
+		//console.debug(val.title);
+	});
+	
+//	console.debug(aaData);
+
+	$('#grid').dataTable({
+		"sPaginationType": "full_numbers",
+		"aaData": aaData
+	});
+	
+	
+//	$('#grid').dataTable({
+//		"sPaginationType": "full_numbers",
+//		"aaData": JSON.stringify($(retorno),null,2)
+//	});
+	
 } 
 
