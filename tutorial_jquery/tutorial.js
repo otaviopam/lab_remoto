048b4edf45cef72d480b48298f171f89dd6edd58
otaviopam <otaviopam@gmail.com>
2013-07-30 18:04:11 -0300

    tutorial

--- tutorial_jquery/tutorial.js
+++ tutorial_jquery/tutorial.js
@@ -1,5 +1,11 @@
 $(function() {
 	$('#tabs').tabs({
-
+		load: function (event,ui) {
+			$('#btnJavascript1').click(function () {
+				$('#exemploJavascript1').slideToggle();
+			});
+		}
 	});
+	
+	
 });
